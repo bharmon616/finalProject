e96d546cb8e7c834816f5740116b4ceb00a2c952
Brett Harmon <BrettHarmon@stmarksschool.org>
2018-05-16 21:57:11 -0400

    I AM A LEGEND

---
+++ js/lezgo.js
@@ -0,0 +1,167 @@
+// OGTewks90 //
+
+
+function initialSetUp(){
+	//alert("hi in initialSetUP");
+	document.getElementById("listBox").style.display = "block";
+	document.getElementById("danceBox").style.display = "none";
+	document.getElementById("danceBox2").style.display = "none";
+	document.getElementById("danceBox3").style.display = "none";
+	document.getElementById("danceBox4").style.display = "none";
+	document.getElementById("danceBox5").style.display = "none";
+	document.getElementById("danceBox6").style.display = "none";
+	document.getElementById("danceBox7").style.display = "none";
+	document.getElementById("danceBox8").style.display = "none";
+	document.getElementById("danceBox9").style.display = "none";
+	document.getElementById("danceBox10").style.display = "none";
+	document.getElementById("danceBox11").style.display = "none";
+	document.getElementById("danceBox12").style.display = "none";
+	document.getElementById("danceBox13").style.display = "none";
+	document.getElementById("danceBox14").style.display = "none";
+	document.getElementById("danceBox15").style.display = "none";
+	document.getElementById("danceBox16").style.display = "none";
+	document.getElementById("danceBox17").style.display = "none";
+	document.getElementById("danceBox18").style.display = "none";
+
+
+
+
+}
+
+
+function dancePopUp(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox").style.display = "block";
+
+}
+
+function dancePopUp2(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox2").style.display = "block";
+
+}
+
+function dancePopUp3(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox3").style.display = "block";
+
+}
+
+function dancePopUp4(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox4").style.display = "block";
+
+}
+
+function dancePopUp5(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox5").style.display = "block";
+
+}
+
+function dancePopUp6(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox6").style.display = "block";
+
+}
+
+function dancePopUp7(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox7").style.display = "block";
+
+}
+
+function dancePopUp8(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox8").style.display = "block";
+
+}
+
+function dancePopUp9(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox9").style.display = "block";
+
+}
+
+function dancePopUp10(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox10").style.display = "block";
+
+}
+
+function dancePopUp11(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox11").style.display = "block";
+
+}
+
+function dancePopUp12(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox12").style.display = "block";
+
+}
+
+function dancePopUp13(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox13").style.display = "block";
+
+}
+
+function dancePopUp14(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox14").style.display = "block";
+}
+
+function dancePopUp15(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox15").style.display = "block";
+}
+
+function dancePopUp16(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox16").style.display = "block";
+}
+
+function dancePopUp17(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox17").style.display = "block";
+}
+
+function dancePopUp18(){
+	//alert(" Hi in dance pop up");
+	document.getElementById("listBox").style.display = "none";
+	document.getElementById("danceBox18").style.display = "block";
+}
+
+
+
+
+
+
+
+
+
+
+
+initialSetUp();
+
+
+
+
